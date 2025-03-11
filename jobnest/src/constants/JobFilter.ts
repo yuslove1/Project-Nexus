45e94d65abc2d338d08b603e5863093e6081a3db
yuslove1 <adexyuslove@gmail.com>
2025-03-11 11:14:12 -0700

    refactor

---
+++ jobnest/src/constants/JobFilter.ts
@@ -0,0 +1,15 @@
+const JOBCATEGORIES = [
+    "All Category",
+    "Engineering",
+    "Design",
+    "Marketing"
+];
+
+const JOBEXPERIENCE = [
+    "Experience Level",
+    "Entry-level",
+    "Mid-level", 
+    "Senior-level"
+]
+
+export { JOBCATEGORIES, JOBEXPERIENCE} 
