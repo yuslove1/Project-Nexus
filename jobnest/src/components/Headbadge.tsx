acdb3a72caff659648388938c5daf7a70c12f8f3
yuslove1 <adexyuslove@gmail.com>
2025-03-11 11:06:06 -0700

    feat: headbadge component for details and filtered result page

---
+++ jobnest/src/components/Headbadge.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BG } from "@/constants/data";
+
+const HeadBadge: React.FC = () => {
+  return (
+    <section className=" min-h-[20vh] bg-blue-200 flex justify-center items-center">
+        <div className="min-full">
+            <h1 className="text-4xl md:text-7xl font-extrabold bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent inline-block mt-2 md:mt-4">Your search</h1>
+        </div>
+    </section>
+  );
+};
+
+export default HeadBadge;
