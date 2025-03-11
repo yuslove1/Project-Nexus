addaf310b617d9de4cada25fe40577a96acf6e7e
yuslove1 <adexyuslove@gmail.com>
2025-03-11 11:01:03 -0700

    configuration and images

--- jobnest/next.config.ts
+++ jobnest/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  // images: {
+  //   domains: ['example.com', 'cdn.example.com', 'res.cloudinary.com'],
+  //   formats: ['image/avif', 'image/webp'],
+  //   minimumCacheTTL: 60,
+  // },
 };
 
 export default nextConfig;
--- jobnest/src/pages/_app.tsx
+++ jobnest/src/pages/_app.tsx
@@ -1,6 +1,21 @@
+import { JobContext, JobProvider } from "@/contexts/JobContext";
+import { FilterProvider } from "@/contexts/FilterContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "@/components/layout/Layout";
+import ApplicationFormModal from "@/components/ApplicationFormModal";
+import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+     <JobProvider>
+      <FilterProvider>
+      <Layout>
+       <Component {...pageProps} />;
+
+       <ApplicationFormModal />
+    </Layout>
+    </FilterProvider>
+    </JobProvider>
+  )
 }
