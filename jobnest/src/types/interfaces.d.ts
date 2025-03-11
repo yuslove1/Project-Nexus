19fcd0d69145aac9a26d649080fd0cc9db685758
yuslove1 <adexyuslove@gmail.com>
2025-03-11 11:32:54 -0700

    perf: interfaces

---
+++ jobnest/src/types/interfaces.d.ts
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+
+export interface ButtonProps{
+    btnStyle: string;
+    btnText: string;
+    onBtnClick?: (id?: string | number ) => void | string | React.ReactElement;
+  }
+  
